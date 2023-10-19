b1b73a1aab3ba066c5fd36466e4dc6bde193523b
Highom <highomlp@gmail.com>
2023-10-19 20:30:21 +0200

    removed header

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
 import './app.css'
 import App from './App.svelte'
-import Header from './lib/Header.svelte'
 
 const app = new App({
   target: document.getElementById('app'),
-})
-
-const header = new Header({
-  target: document.getElementById('header'),
 })
 
 export default app
