c45e3de55a0c7379aff5b6d6095a9176d7af260c
Yannick Ruck <yannick.ruck@vgt.energy>
2026-04-08 21:20:22 +0200

    Updates

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.SB_SITE_CONFIG = {
   supabaseUrl: "https://gjhnqhijtyrhzynhpbmc.supabase.co",
   supabasePublishableKey: "sb_publishable_i4FRid5xYkvVXDDtdcF3WQ_V1HJs5Gj",
-  // turnstileSiteKey: "0x4AAAAAAC2Ese3cDPOgMbex",
-  turnstileSiteKey: "1x00000000000000000000AA", // testing key
+  turnstileSiteKey: "0x4AAAAAAC2Ese3cDPOgMbex",
+  // turnstileSiteKey: "1x00000000000000000000AA", // testing key
 };
